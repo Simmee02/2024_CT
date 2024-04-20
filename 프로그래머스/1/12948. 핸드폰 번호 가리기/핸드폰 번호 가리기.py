ede17af634f373e20c2114e99697d8dfa5f16673
Simmee02 <114573447+Simmee02@users.noreply.github.com>
2024-04-20 17:50:49 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -0,0 +1,7 @@
+def solution(phone_number):
+    answer = '*'*(len(phone_number)-4)
+    for i in range(len(phone_number)-4,len(phone_number)):
+        answer = answer + phone_number[i]
+        
+    
+    return answer
