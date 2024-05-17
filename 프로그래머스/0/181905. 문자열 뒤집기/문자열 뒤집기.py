46e47a58da7338ab5e08dbdba742dfeca411a6c2
Simmee02 <114573447+Simmee02@users.noreply.github.com>
2024-05-17 19:59:48 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.py
@@ -0,0 +1,11 @@
+def solution(my_string, s, e):
+    answer = ''
+    reverse = ''
+    
+    reverse = my_string[s:e+1]
+    reverse = reverse[::-1]
+    
+    answer = my_string[:s] + reverse + my_string[e+1:]
+    
+    
+    return answer
