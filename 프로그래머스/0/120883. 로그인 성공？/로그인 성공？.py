54caa9c79977e4e6f1e859612045de7aecee4a2d
Simmee02 <114573447+Simmee02@users.noreply.github.com>
2024-05-18 06:03:29 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,11 @@
+def solution(id_pw, db):
+    answer = ''
+    
+    for i in range(len(db)):
+        if db[i][0] == id_pw[0]:
+            if db[i][1] == id_pw[1]:
+                return "login"
+            else:
+                return "wrong pw"
+    
+    return "fail"
