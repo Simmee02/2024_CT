4178c44f49f989e6ccef7c66cd582a6ef4641c81
Simmee02 <114573447+Simmee02@users.noreply.github.com>
2024-06-22 23:13:59 +0900

    [level 0] Title: 모스부호 (1), Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.py
@@ -0,0 +1,13 @@
+def solution(letter):
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    morse_list = letter.split(' ')
+    decoded_chars = [morse[code] for code in morse_list]
+    
+    
+    return ''.join(decoded_chars)
